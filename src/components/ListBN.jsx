752e744574b557cac685ce51dbd5f56e1693b815
Imperfects <76396996+hoan3232@users.noreply.github.com>
2023-01-13 23:14:29 +0700

    all display function

--- src/components/ListBN.jsx
+++ src/components/ListBN.jsx
@@ -1,8 +1,18 @@
-import React, { useContext } from "react";
-import TodoAppContext from "../todoAppContext.js";
+import React, { useEffect, useState } from "react";
+import { instance } from "../utils";
 
 function ListBN(props) {
-  const { store } = useContext(TodoAppContext);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const getDebt = async () => {
+      const res = await instance.get(
+        `users/debt/${localStorage.todoApp_userSTK}`
+      );
+      setList(res.data);
+    };
+    getDebt();
+  }, []);
+  let count = 0;
 
   return (
     <div>
@@ -17,13 +27,38 @@ function ListBN(props) {
             <th>Nội dung</th>
             <th>Trạng thái</th>
           </tr>
+          {list.map((val, key) => {
+            let date = new Date(val.Ngay_Gio);
+
+            let datef =
+              date.getFullYear() +
+              "-" +
+              (date.getMonth() + 1) +
+              "-" +
+              date.getDate() +
+              " " +
+              ("0" + date.getHours()).slice(-2) +
+              ":" +
+              ("0" + date.getMinutes()).slice(-2) +
+              ":" +
+              ("0" + date.getSeconds()).slice(-2);
+            let dateString = datef.toString();
+            return (
+              <tr key={key}>
+                <td>{++count}</td>
+                <td>{dateString}</td>
+                <td>{val.Ma_Ng_Nhan}</td>
+                <td>{val.TK_TT_DS_NN_Ma_Ng_GuiToTK_TT.DS_TK.Ten_DK}</td>
+                <td>{val.So_No}</td>
+                <td>{val.Noi_Dung}</td>
+                <td>
+                  <button onClick={() => handleInfo(val)}>Chi tiết</button>
+                </td>
+              </tr>
+            );
+          })}
         </table>
       </div>
-      <ul>
-        {store.items.map(function (item) {
-          return <TodoItem key={item.id} task={item} />;
-        })}
-      </ul>
     </div>
   );
 }
